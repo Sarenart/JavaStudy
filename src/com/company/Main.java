c4909e4c7096bec53abe34cdfcdb73efc7ed2571
kotenev.tosha mail.ru <kotenev.tosha@mail.ru>
2021-01-16 11:45:46 +0300

    New Commit fot practice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,27 +16,29 @@ public class Main {
         else
         {
             System.out.println("Не норм");
-        }*/
-       /* for (int i = 0; i < 10; i++){
+        }
+        for (int i = 0; i < 10; i++){
             System.out.println(i);
         }
         int j = 0;
         while(j < 10){
             System.out.println(j);
             j++;
-        }*/
+        }
         float [] name = new float[]{23f, 45f, 63f};
         System.out.println(name[2]);
         char[][] array = new char[3][2];
         array[2][1] = 'A';
         System.out.println(array[2][1]);
         System.out.println(func(4,5));
-        //int[] a = readArray();
-       // for(int i = 0; i< a.length; i++){
-       //     System.out.println("The " + (i+1)+ "'s int is "+a[i]);
-       // }
+        int[] a = readArray();
+        for(int i = 0; i< a.length; i++){
+            System.out.println("The " + (i+1)+ "'s int is "+a[i]);
+        }
         ShowVariableLength(1,5,3,2);
-        ShowVariableLength(8,4);
+        ShowVariableLength(8,4);*/
+        Person NewPerson = new Person();
+        System.out.println(NewPerson.height);
     }
     public static int func(int a, int b){
         return a+b;
